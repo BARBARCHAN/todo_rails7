e097aec7c7749b6f0e54c463867489fb4bf6538b
JUNSHIROBABA-pv <70783556+JUNSHIROBABA-pv@users.noreply.github.com>
2023-05-21 20:08:15 +0900

    [Add]post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,17 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_path
   end
 
   def edit
@@ -19,6 +24,8 @@ class PostsController < ApplicationController
   end
 
   private
-
+    def post_params
+      params.require(:post).permit(:text)
+    end
   
 end
